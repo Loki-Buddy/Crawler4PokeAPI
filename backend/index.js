b7214178952e0f74c718c4d0c2f06aa1a23a91f7
Loki-Buddy <jm.conring@live.de>
2025-05-05 10:27:02 +0200

    Komentare hinzugefügt

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+// Verbindung zur Datenbank herstellen
 const pool = new Pool(
     {
         user: process.env.DB_USER, // Dein PostgreSQL-Benutzername
@@ -14,30 +15,40 @@ const pool = new Pool(
     }
 );
 
+// Pokedex-API initialisieren
 const P = new Pokedex();
 
+// die Tabelle Pokedex.pokemon mit den Daten der Pokémon-API befuellen
 (async () => {
     try {
         const interval = { limit: 151, offset: 0 };
-        P.getPokemonsList(interval)
+        P.getPokemonsList(interval) // alle Pokémon abrufen
             .then(async (response) => {
-                //console.log(response.results[1].name);
+                // die Daten der Pokémon in die Tabelle schreiben
                 for (let i = 0; i < 151; i++) {
+                    //  Fetch auf die Species um auf die Namen in deutscher Sprache zu kommen
                     const original = await P.getPokemonSpeciesByName(response.results[i].name);
                     const ger = original.names.filter((pokeAPIName) => pokeAPIName.language.name === 'de')[0].name;
+
+                    // Fetch auf die Pokémon um die Sprites zu bekommen
                     const pokemon = await P.getPokemonByName(response.results[i].name);
                     const front_sprites_url = pokemon.sprites.front_default;
-                    const client = await pool.connect();
+
+                    // Daten in die Tabelle schreiben
+                    const client = await pool.connect(); // Verbindung zur Datenbank herstellen
                     try {
+                        // Setzt den Schema auf "Pokedex"
                         await client.query('SET search_path TO "Pokedex";');
+
+                        // INSERT Befehl um die Daten in die Tabelle zu schreiben
                         await client.query('INSERT INTO pokemon (name, front_sprites) VALUES ($1, $2);', [ger, front_sprites_url]);
                     } catch (error) {
                         console.error('Fehler beim Abrufen der Pokémon-Daten:', error);
                     } finally {
+                        // Verbindung zur Datenbank schliessen bzw freigeben
                         client.release();
                     }
                 }
-
             });
     } catch (error) {
         console.error('Fehler beim Abrufen der Pokémon-Daten:', error);
