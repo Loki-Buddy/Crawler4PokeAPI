ebc0533c87dba4cd7ea3aa1bd743fec18c969bdc
Loki-Buddy <jm.conring@live.de>
2025-05-14 12:21:49 +0200

    FERTIG!

--- backend/battlemechanic/5bmInsertWeekStrong.js
+++ backend/battlemechanic/5bmInsertWeekStrong.js
@@ -15,3 +15,59 @@ const pool = new Pool({
 
 const P = new Pokedex(); // Initialisierung der Pokedex-API
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+(async () => {
+    try {
+        const allPokemon = await P.getPokemonsList({ limit: 151, offset: 0 });
+        let weak = [];
+        let strong = [];
+
+        for (let i = 0; i < allPokemon.results.length; i++) {
+            const this_pokemon = await P.getPokemonByName(allPokemon.results[i].name);
+            const type_slot1 = await P.getTypeByName(this_pokemon.types[0].type.name);
+            const type_slot2 = this_pokemon.types[1] ? await P.getTypeByName(this_pokemon.types[1].type.name) : null; // Zweiter Typ (falls vorhanden)
+
+            for (let j = 0; j < type_slot1.damage_relations.double_damage_from.length; j++) {
+                weak.push(type_slot1.damage_relations.double_damage_from[j].name);
+                console.log(type_slot1.damage_relations.double_damage_from[j].name + ' ist schwach gegen ' + this_pokemon.types[0].type.name);
+            }
+
+            for (let j = 0; j < type_slot1.damage_relations.double_damage_to.length; j++) {
+                strong.push(type_slot1.damage_relations.double_damage_to[j].name)
+                console.log(type_slot1.damage_relations.double_damage_to[j].name + ' ist stark gegen ' + this_pokemon.types[0].type.name);
+            }
+
+            if (type_slot2 !== null) {
+                for (let j = 0; j < type_slot2.damage_relations.double_damage_from.length; j++) {
+                    weak.push(type_slot2.damage_relations.double_damage_from[j].name);
+                    console.log(type_slot2.damage_relations.double_damage_from[j].name + ' ist schwach gegen ' + this_pokemon.types[1].type.name);
+                }
+
+                for (let j = 0; j < type_slot2.damage_relations.double_damage_to.length; j++) {
+                    strong.push(type_slot2.damage_relations.double_damage_to[j].name);
+                    console.log(type_slot2.damage_relations.double_damage_to[j].name + ' ist stark gegen ' + this_pokemon.types[1].type.name);
+                }
+            }
+
+            weak = [...new Set(weak)];
+            strong = [...new Set(strong)];
+
+            const client = await pool.connect();
+            try {
+                await client.query(`SET search_path TO "BattleMechanic";`)
+                const result = await client.query(`SELECT id FROM typ WHERE api_name = $1;`, [this_pokemon.types[0].type.name]);
+                await client.query(`INSERT INTO pokemon_weak_strong (typ_id, weak, strong) VALUES ($1, $2, $3)`, [result.rows[0].id, weak, strong]);
+
+                console.log('Eintrag wurde erfolgreich gespeichert!');
+            } catch (error) {
+                console.error(error);
+            } finally {
+                client.release();
+            }
+
+        }
+    } catch (error) {
+        console.error('Fehler! Error: ', error)
+    }
+
+})();
